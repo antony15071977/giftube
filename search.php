911b3f6b4cb84f8a2cf3feea144720f08bece713
Antony1507 <i.avraamy2@gmail.com>
2020-11-11 18:43:11 +0200

    Пагинация

--- search.php
+++ search.php
@@ -6,82 +6,86 @@ $connect = mysqli_connect("localhost", "root", "", "giftube");
 mysqli_set_charset($connect, "utf8");
 
 if(!$connect) {
-    print('Ошибка подключения: ' . mysqli_connect_error());
+	print('Ошибка подключения: ' . mysqli_connect_error());
 } else {
 
     // 1. запрос для получения списка категорий;
-    $sql_cat = 'SELECT * FROM categories';
-    $res_cat = mysqli_query($connect, $sql_cat);
-    if($res_cat) {
-        $categories = mysqli_fetch_all($res_cat, MYSQLI_ASSOC);
-    } else {
-        $error = mysqli_error($connect);
-        print('Ошибка MySQL: ' . $error);
-    }
+	$sql_cat = 'SELECT * FROM categories';
+	$res_cat = mysqli_query($connect, $sql_cat);
+	if($res_cat) {
+		$categories = mysqli_fetch_all($res_cat, MYSQLI_ASSOC);
+	} else {
+		$error = mysqli_error($connect);
+		print('Ошибка MySQL: ' . $error);
+	}
     // 2. запрос для получения списка гифок
-    $gifs = [];
-    $search = isset($_GET['q']) ? $_GET['q'] : '';
+	$gifs = [];
+	$search = isset($_GET['q']) ? $_GET['q'] : '';
     //переменная для получения логического списка гифок
-    $search = $search . '*'; 
-     if ($search) {        
+	$search = $search . '*'; 
+	if ($search) {        
+		$res_count_gifs = mysqli_query($connect, 'SELECT count(*) AS cnt FROM gifs WHERE MATCH(title, description) AGAINST(' . '"'. $search . '"' .'IN BOOLEAN MODE)' );
+		$items_count = mysqli_fetch_assoc($res_count_gifs)['cnt'];		
 
-        $sql_gifs = 'SELECT g.id, g.dt_add, category_id, user_id, title, description, img_path, likes_count, favs_count, views_count, u.name ' .
-            'FROM gifs g ' .
-            'JOIN users u ON g.user_id = u.id ' .
-            'JOIN categories c ON g.category_id = c.id ' .
-            'WHERE MATCH(title, description) AGAINST(? IN BOOLEAN MODE) ' .
-            'ORDER BY g.dt_add DESC';
-        $stmt = db_get_prepare_stmt($connect, $sql_gifs, [$search]);
-        mysqli_stmt_execute($stmt);
-        $res = mysqli_stmt_get_result($stmt);
-        if ($res) {
-            $gifs = mysqli_fetch_all($res, MYSQLI_ASSOC);
+		$current_page = isset($_GET['page']) ? $_GET['page'] : 1;
+		//сколько позиций на странице
+		$page_items = 3;
+		$offset = ($current_page - 1) * $page_items;
+		$pages_count = ceil($items_count / $page_items);
+		$pages = range(1, $pages_count);
 
-	        $res_count_gifs = mysqli_query($connect, 'SELECT count(*) AS cnt FROM gifs WHERE MATCH(title, description) AGAINST(' . '"'. $search . '"' .')');
-	        $items_count = mysqli_fetch_assoc($res_count_gifs)['cnt'];
-
-	        if (!$items_count) {
-	        	$page_content = include_template('main.php',
-	        		['gifs' => $gifs,
-	    			'title' => 'Ничего не найдено'
-				]);
-	        }
-	        else {
-	        	$page_content = include_template('main.php', [
-	    			'gifs' => $gifs,
-	    			'title' => 'Результаты поиска'
-				]);
-	        }
-     
-    	}
-    	else {
-    	$page_content = include_template('main.php', [
-    		'gifs' => $gifs,
-    		'title' => 'Ничего не найдено'
-			]);
-    	}
-    }
-    
+		$sql_gifs = 'SELECT g.id, g.dt_add, category_id, user_id, title, description, img_path, likes_count, favs_count, views_count, u.name ' .
+		'FROM gifs g ' .
+		'JOIN users u ON g.user_id = u.id ' .
+		'JOIN categories c ON g.category_id = c.id ' .
+		'WHERE MATCH(title, description) AGAINST(? IN BOOLEAN MODE) ' .
+		'ORDER BY g.dt_add DESC LIMIT ' . $page_items . ' OFFSET ' . $offset;
+		$stmt = db_get_prepare_stmt($connect, $sql_gifs, [$search]);
+		mysqli_stmt_execute($stmt);
+		$res = mysqli_stmt_get_result($stmt);
+		if ($res) {
+			$gifs = mysqli_fetch_all($res, MYSQLI_ASSOC);
+		}
+	}
+	$param = isset($_GET['q']) ? ('q=' . $_GET['q'] . '&') : '';
 }
 
-if (isset($_SESSION['user'])) {
+$pagination = include_template('pagination.php', [
+	'param' => $param,
+	'pages_count' => $pages_count,
+	'pages' => $pages,
+	'current_page' => $current_page
+]);
 
-    $layout_content = include_template('layout.php', [
-        'username' => $_SESSION['user']['name'],
-        'content' => $page_content,
-        'categories' => $categories,
-        'title' => 'Результаты поиска',
-        'search' => $search
+if (!$items_count) {
+    $page_content = include_template('main.php', [
+    	'gifs' => $gifs,
+    	'title' => 'Ничего не найдено'
     ]);
 }
 else {
-
-    $layout_content = include_template('layout.php', [
-        'content' => $page_content,
-        'categories' => $categories,
-        'title' => 'Результаты поиска',
-        'search' => $search
-    ]);
+   $page_content = include_template('main.php', [
+	'gifs' => $gifs,
+	'title' => 'Результаты поиска',
+	'pagination' => $pagination
+	]);
 }
 
+if (isset($_SESSION['user'])) {
+	$layout_content = include_template('layout.php', [
+		'username' => $_SESSION['user']['name'],
+		'content' => $page_content,
+		'categories' => $categories,
+		'title' => 'Результаты поиска',
+		'search' => $search
+	]);
+}
+else {
+	$layout_content = include_template('layout.php', [
+		'content' => $page_content,
+		'categories' => $categories,
+		'title' => 'Результаты поиска',
+		'search' => $search
+	]);
+}
 print($layout_content);
